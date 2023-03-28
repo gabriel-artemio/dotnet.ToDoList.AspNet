0c5b34d21db5d75c226bc16ee3d5ffcc5386954c
Gabriel Artemio <43358004+gabriel-artemio@users.noreply.github.com>
2023-03-28 14:06:15 -0300

    criacao model todo

---
+++ Models/Todo.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace TodoListAsp.Models
+{
+    public class Todo
+    {
+        public int Id { get; set; }
+
+        [DisplayName("Título")]
+        [Required(ErrorMessage = "Campo obrigatório")]
+        public string Titulo { get; set; }
+
+        [DisplayName("Status")]
+        public bool Concluido { get; set; }
+
+        [DisplayName("Data de criação")]
+        public DateTime CriadoEm { get; set; } = DateTime.Now;
+
+        [DisplayName("Última atualização")]
+        public DateTime UltimaAtualizacao { get; set; } = DateTime.Now;
+
+        [DisplayName("Usuário")]
+        public int Usuario { get; set; }
+    }
+}
